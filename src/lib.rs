554297a2ef75f075c53890e8efd0948e80b80f62
Paul Schifferer <paul@schifferers.net>
2025-01-04 14:20:12 -0800

    Add changelog

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
-/// This is the main library file for the sweetrpg-model-core library.
+/**
+ * This is the main library file for the sweetrpg-model-core library.
+ * @paulyhedral
+ */
 pub mod models;
 pub mod vo;
